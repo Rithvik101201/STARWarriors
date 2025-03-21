5a23ccf6306b8839df485d24a273215c05cc1ffa
rithvik <rithvik10.potana@gmail.com>
2025-03-21 19:24:55 +0530

    server js file added

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3000;
+
+const folderPath = path.join(__dirname,'public')
+
+app.use(express.static(folderPath))
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join('folderPath/index.html'))
+})
+
+app.listen(PORT, (err)=>{
+    if(err) throw err
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
